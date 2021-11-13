d8866315c3809e2b65c286d2ee0dc60dd35bf6c1
g-marshak <marshak22@up.edu>
2021-11-13 00:35:39 -0800

    Created basic script to run server/client from one place (needs testing with actual RSA)

---
+++ rsa_dpx/mainscript.py
@@ -0,0 +1,21 @@
+#script to spawn 2 threads, run rsa_dpx in one and clientdemo in the other
+from time import sleep, perf_counter
+from threading import Thread
+
+def server():
+    exec(open("rsa_dpx.py").read())
+
+def client():
+    exec(open("clientdemo.py").read())
+
+#create threads
+t1 = Thread(target=server)
+t2 = Thread(target=client)
+
+#start threads
+t1.start()
+t2.start()
+
+#join threads
+t1.join()
+t2.join()
